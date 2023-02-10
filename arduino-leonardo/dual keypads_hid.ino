0d981bd1cedb8b13a1b731f55900c1682eac68dc
BabaJeanmel <32860682+BabaJeanmel@users.noreply.github.com>
2023-02-10 18:26:20 +0100

    Add files via upload

--- arduino-leonardo/dual keypads_hid.ino
+++ arduino-leonardo/dual keypads_hid.ino
@@ -4,43 +4,59 @@
 char previousPressedKey;
 boolean hasReleasedKey = false;
 
-const byte ROWS1 = 4; //four rows
-const byte COLS1 = 3; //three columns
+const byte ROWS1 = 4;  //four rows
+const byte COLS1 = 3;  //three columns
 char keys1[ROWS1][COLS1] = {
-  {'1','2','3'},
-  {'4','5','6'},
-  {'7','8','9'},
-  {'+','0','*'}
+  { '\341', '\342', '\343' },
+  { '\344', '\345', '\346' },
+  { '\347', '\350', '\351' },
+  { '\335', '\353', '\336' }
 };
-byte rowPins1[ROWS1] = { 8, 7, 6, 5 };    //connect to the row pinouts of the keypad
-byte colPins1[COLS1] = { 4, 3, 2 };  //connect to the column pinouts of the keypad
+byte rowPins1[ROWS1] = { 8, 7, 6, 5 };  //connect to the row pinouts of the keypad
+byte colPins1[COLS1] = { 4, 3, 2 };     //connect to the column pinouts of the keypad
 
-Keypad keypad1 = Keypad( makeKeymap(keys1), rowPins1, colPins1, ROWS1, COLS1 );
+Keypad keypad1 = Keypad(makeKeymap(keys1), rowPins1, colPins1, ROWS1, COLS1);
 
-const byte ROWS2 = 4; //four rows
-const byte COLS2 = 3; //three columns
+char
+    keyPressed1,
+    lastKey1;
+
+const byte ROWS2 = 4;  //four rows
+const byte COLS2 = 3;  //three columns
 char keys2[ROWS2][COLS2] = {
-  {'r','t','y'},
-  {'f','g','h'},
-  {'v','b','n'},
-  {'m','u','j'}
+  { 'r', 't', 'y' },
+  { 'f', 'g', 'h' },
+  { 'v', 'b', 'n' },
+  { 'm', 'u', 'j' }
 };
 byte rowPins2[ROWS2] = { A1, A0, 13, 12 };  //connect to the row pinouts of the keypad
 byte colPins2[COLS2] = { 11, 10, 9 };       //connect to the column pinouts of the keypad
 
-Keypad keypad2 = Keypad( makeKeymap(keys2), rowPins2, colPins2, ROWS2, COLS2 );
+Keypad keypad2 = Keypad(makeKeymap(keys2), rowPins2, colPins2, ROWS2, COLS2);
+
+char
+    keyPressed2,
+    lastKey2;
 
 //update instances and possibly fire funcitons
 
-void setup(){
+char holdKey;
+unsigned long t_hold;
+
+void setup() {
   Serial.begin(9600);
+  Keyboard.begin();
+  keypad1.setHoldTime( 200 );
+  keypad2.setHoldTime( 200 );
+  lastKey1 = keypad1.getKey();  
+  lastKey2 = keypad2.getKey();  
   delay(1000);
 }
 
-void loop(){
-delay(50);
-keypadLoop1();
-keypadLoop2();
+void loop() {
+  delay(50);
+  keypadLoop1();
+  keypadLoop2();
 }
 
 void keypadLoop1() {
@@ -48,22 +64,25 @@ void keypadLoop1() {
   KeyState state1 = keypad1.getState();
 
   if (state1 == PRESSED && key1 != NO_KEY) {
-  previousPressedKey = key1;
-  hasReleasedKey = false;
-  Keyboard.print(key1);
-}
+    previousPressedKey = key1;
+    hasReleasedKey = false;
+    Keyboard.press(key1);
+    delay(100);
+    Keyboard.release(key1);
+  }
 
-else if (state1 == RELEASED && !hasReleasedKey) {
-// Multiple RELEASED events occur when there had not been HOLD
- Keyboard.print(key1);
- hasReleasedKey = true;
+  else if (state1 == RELEASED && !hasReleasedKey) {
+    // Multiple RELEASED events occur when there had not been HOLD
+    Keyboard.press(key1);
+    hasReleasedKey = true;
+  }
 
-}
-else if (state1 == HOLD) {
-Keyboard.print(key1);
-Keyboard.println(previousPressedKey);
 
-}
+//  else if (state1 == HOLD) {
+//    Keyboard.print(key1);
+//    Keyboard.print(previousPressedKey);
+//  }
+
 }
 
 void keypadLoop2() {
@@ -71,20 +90,21 @@ void keypadLoop2() {
   KeyState state2 = keypad2.getState();
 
   if (state2 == PRESSED && key2 != NO_KEY) {
-  previousPressedKey = key2;
-  hasReleasedKey = false;
-  Keyboard.print(key2);
-}
+    previousPressedKey = key2;
+    hasReleasedKey = false;
+    Keyboard.press(key2);
+    delay(100);
+    Keyboard.release(key2);
+  }
 
-else if (state2 == RELEASED && !hasReleasedKey) {
-// Multiple RELEASED events occur when there had not been HOLD
- Keyboard.print(key2);
- hasReleasedKey = true;
+  else if (state2 == RELEASED && !hasReleasedKey) {
+    // Multiple RELEASED events occur when there had not been HOLD
+    Keyboard.press(key2);
+    hasReleasedKey = true;
+  }
 
-}
-else if (state2 == HOLD) {
-Keyboard.print(key2);
-Keyboard.println(previousPressedKey);
-
-}
+//  else if (state2 == HOLD) {
+//    Keyboard.print(key2);
+//    Keyboard.print(previousPressedKey);
+//  }
 }
